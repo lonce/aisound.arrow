3f18077884f762c6b58dbfdd54b8dd3b25126916
mclonce <lonce.wyse@nus.edu.sg>
2019-03-07 13:07:39 +0800

    first commit

--- appscripts/main.js
+++ appscripts/main.js
@@ -2,7 +2,7 @@
 A Demo of using an aiSound with parameters tied to an animated graphical object.
 */
 
-import sndFactory from 'https://aisound.cloud/aiSounds/fm2.js'
+import sndFactory from 'https://aisound.cloud/aiSounds/swish.js'
 //import sndFactory from 'http://localhost:8000/aiSounds/swish.js'
 
 var snd;
